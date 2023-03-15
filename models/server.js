1cd70d438bef2e3f86443417c6f58ab7258ba043
Alonso Manilla <alonso.manilla@gmail.com>
2023-03-14 20:14:08 -0600

    jwt

--- models/server.js
+++ models/server.js
@@ -3,45 +3,47 @@ const cors = require('cors');
 const { dbConnection } = require('../database/config');
 
 class Server {
-  constructor() {
-    this.app = express();
-    this.port = process.env.PORT;
-    this.usuariosPath = '/api/usuarios';
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+        this.authPath = '/api/auth';
 
-    // Conectar a base de datos
-    this.conectarDB();
+        // Conectar a base de datos
+        this.conectarDB();
 
-    // Middlewares
-    this.middlewares();
+        // Middlewares
+        this.middlewares();
 
-    // Rutas de mi aplicación
-    this.routes();
-  }
+        // Rutas de mi aplicación
+        this.routes();
+    }
 
-  async conectarDB() {
-    await dbConnection();
-  }
+    async conectarDB() {
+        await dbConnection();
+    }
 
-  middlewares() {
-    //CORS
-    this.app.use(cors());
+    middlewares() {
+        //CORS
+        this.app.use(cors());
 
-    // Lectura y parseo del body
-    this.app.use(express.json());
+        // Lectura y parseo del body
+        this.app.use(express.json());
 
-    // Directorio publico
-    this.app.use(express.static('public'));
-  }
+        // Directorio publico
+        this.app.use(express.static('public'));
+    }
 
-  routes() {
-    this.app.use(this.usuariosPath, require('../routes/usuarios'));
-  }
+    routes() {
+        this.app.use(this.authPath, require('../routes/auth'));
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
+    }
 
-  listen() {
-    this.app.listen(this.port, () => {
-      console.log('Servidor corriendo en puerto: ', this.port);
-    });
-  }
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto: ', this.port);
+        });
+    }
 }
 
 module.exports = Server;
